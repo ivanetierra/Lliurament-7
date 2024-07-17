86daf18e171eeebff41ab755597d9bc777ee6bdf
ielizalde <ielizalde99@gmail.com>
2024-07-17 12:40:18 +0200

    add styling and routes to the navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StarshipsComponent } from './components/starships/starships.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'starships', component: StarshipsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+]
