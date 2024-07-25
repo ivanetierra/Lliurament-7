3fe0d731cffd2f09a61574b4858d8987b1fc47d6
ielizalde <ielizalde99@gmail.com>
2024-07-25 13:06:35 +0200

    add infinite scrool for showing starships

--- src/app/components/starships/starships.component.ts
+++ src/app/components/starships/starships.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StarshipService } from '../../services/starship.service';
 
@@ -11,13 +11,35 @@ import { StarshipService } from '../../services/starship.service';
 })
 export class StarshipsComponent implements OnInit {
   starships: any[] = [];
+  loading: boolean = false;
 
   constructor(private starshipService: StarshipService, private router: Router) { }
 
   ngOnInit(): void {
-    this.starshipService.getStarships().subscribe(data => {
+    this.loading = true;
+    this.starshipService.getStarships(1).subscribe(data => {
       this.starships = data.results;
+      this.loading = false;
     });
+  }
+
+  loadMoreStarships() {
+    if (this.loading) return;
+    this.loading = true;
+    this.starshipService.getNextPage().subscribe(data => {
+      this.starships = [...this.starships, ...data.results];
+      this.loading = false;
+    });
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    const pos = (document.documentElement.scrollTop || document.body.scrollTop) + window.innerHeight;
+    const max = document.documentElement.scrollHeight;
+
+    if (pos >= max - 10) {
+      this.loadMoreStarships();
+    }
   }
 
   viewStarship(starship: any): void {
--- src/app/services/starship.service.ts
+++ src/app/services/starship.service.ts
@@ -9,10 +9,12 @@ export class StarshipService {
   private apiUrl = 'https://swapi.dev/api/starships';
   private imgUrl: string = 'https://starwars-visualguide.com/assets/img/starships/';
 
+  private currentPage = 1;
+
   constructor(private http: HttpClient) {}
 
-  getStarships(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+  getStarships(currentPage: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?page=${currentPage}`);
   }
 
   getStarship(id: number): Observable<any> {
@@ -21,5 +23,10 @@ export class StarshipService {
 
   getStarshipImgUrl(id: number): string {
     return `${this.imgUrl}${id}.jpg`;
+  }
+
+  getNextPage(): Observable<any> {
+    this.currentPage++;
+    return this.getStarships(this.currentPage);
   }
 }
